b50819c1a2de2a71675a51ffb43e508ab8f3bd7c
jihun.bang <jihun.bang@doosan.com>
2018-05-18 16:34:31 +0900

    챗봇

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
+//작은 따옴표 사이에 본인이 받으신 token을 paste합니다.
+//나중에 보안을 위해서 따로 setting을 하는 방법을 알려드리겠습니다.
+//이 토큰이 포함된 파일을 절대 업로드하거나 github에 적용시키지 마세요.
+var PAGE_ACCESS_TOKEN = '';
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-    res.send('Hello world');
+    res.send('Hello world, I am a chat bot');
 })
+
 
 app.get('/webhook', function(req, res) {
     if (req.query['hub.verify_token'] === 'VERIFY_TOKEN') {
@@ -20,6 +26,97 @@ app.get('/webhook', function(req, res) {
     }
     res.send('Error, wrong token');
 })
+
+app.post("/webhook", function(req, res) {
+    console.log("WEBHOOK GET IT WORKS");
+    var data = req.body;
+    console.log(data);
+
+    // Make sure this is a page subscription
+    if (data.object == 'page') {
+        // Iterate over each entry
+        // There may be multiple if batched
+        data.entry.forEach(function(pageEntry) {
+            var pageID = pageEntry.id;
+            var timeOfEvent = pageEntry.time;
+
+            // Iterate over each messaging event
+            pageEntry.messaging.forEach(function(messagingEvent) {
+                if (messagingEvent.optin) {
+                    receivedAuthentication(messagingEvent);
+                } else if (messagingEvent.message) {
+                    receivedMessage(messagingEvent);
+                } else if (messagingEvent.postback) {
+                    receivedPostback(messagingEvent);
+                } else {
+                    console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+                }
+            });
+        });
+
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know you've 
+        // successfully received the callback. Otherwise, the request will time out.
+        res.sendStatus(200);
+    }
+});
+
+function receivedMessage(event) {
+    console.log("RECEIVED MESSAGE IT WORKS");
+    var senderId = event.sender.id;
+    var content = event.message.text;
+
+    console.log(content);
+
+    var echo_message = "ECHO : " + content;
+    console.log(echo_message);
+    sendTextMessage(senderId, echo_message);
+}
+
+/*
+ * Postback Event
+ *
+ * This event is called when a postback is tapped on a Structured Message. 
+ * https://developers.facebook.com/docs/messenger-platform/webhook-reference/postback-received
+ * 
+ */
+function receivedPostback(event) {
+    console.log("RECEIVED POSTBACK IT WORKS");
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfPostback = event.timestamp;
+
+    // The 'payload' param is a developer-defined field which is set in a postback 
+    // button for Structured Messages. 
+    var payload = event.postback.payload;
+
+    console.log("Received postback for user %d and page %d with payload '%s' " +
+        "at %d", senderID, recipientID, payload, timeOfPostback);
+
+    // When a postback is called, we'll send a message back to the sender to 
+    // let them know it was successful
+    sendTextMessage(senderID, "Postback called");
+}
+
+// sends message to user
+function sendTextMessage(recipientId, message) {
+    console.log("SEND MESSAGE");
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: PAGE_ACCESS_TOKEN },
+        method: 'POST',
+        json: {
+            recipient: { id: recipientId },
+            message: { text: message }
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ' + response.error);
+        }
+    });
+    console.log("REQUEST END");
+}
 
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'));
